4c49a52c68f3e9666413e2d34af1f8eb18239e5a
gagan3120 <gagan3120@gmail.com>
2021-03-29 16:05:27 +0530

    Update ICarServiceImpl.java

--- Car/src/main/java/net/santosh/springboot/service/ICarServiceImpl.java
+++ Car/src/main/java/net/santosh/springboot/service/ICarServiceImpl.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import net.santosh.springboot.exception.ModelAddException;
+import net.santosh.springboot.exception.ModelEmptyListException;
+import net.santosh.springboot.exception.ModelNotFoundException;
+import net.santosh.springboot.exception.ModelUpdateException;
 import net.santosh.springboot.exception.ResourceNotFoundException;
 import net.santosh.springboot.model.Car;
 import net.santosh.springboot.repository.IAppointmentRepository;
@@ -21,8 +25,11 @@ public class ICarServiceImpl implements ICarService{
 
 	@Override
 	public Car addCar(Car car) {
-		// TODO Auto-generated method stub
-		return ICarRepository.save(car);
+		try {
+			return ICarRepository.save(car);
+		} catch (Exception e) {
+			throw new ModelAddException("Can't add the model Car, please add the proper details");
+		}
 	}
 
 	@Override
@@ -40,53 +47,75 @@ public class ICarServiceImpl implements ICarService{
 	@Override
 	public Car updateCar(Car car, long id) {
 		Optional<Car> carList = this.ICarRepository.findById(id);
+		try {
+			if(carList.isPresent()) {
+				Car carUpdate = carList.get();
+				carUpdate.setBrand(car.getBrand());
+				carUpdate.setModel(car.getModel());
+				carUpdate.setVariant(car.getVariant());
+				carUpdate.setRegistrationYear(car.getRegistrationYear());
+				carUpdate.setRegistrationState(car.getRegistrationState());
+				
+				ICarRepository.save(carUpdate);
+				return carUpdate;
+				}
+			else {
+				throw new ResourceNotFoundException("Record not found with ID :" + id);
+			}
+		}
+		catch(Exception e){
+			throw new ModelUpdateException("couldnt update the appointment details,please try again " );
+		}
 		
-		if(carList.isPresent()) {
-			Car carUpdate = carList.get();
-			carUpdate.setBrand(car.getBrand());
-			carUpdate.setModel(car.getModel());
-			carUpdate.setVariant(car.getVariant());
-			carUpdate.setRegistrationYear(car.getRegistrationYear());
-			carUpdate.setRegistrationState(car.getRegistrationState());
-			
-			ICarRepository.save(carUpdate);
-			return carUpdate;
-		}
-		else {
-			throw new ResourceNotFoundException("Record not found with ID :" + id);
-		}
 		
 	}
 
 	@Override
 	public Car getCar(long id) {
 		Optional<Car> carList = this.ICarRepository.findById(id);
-		
-		if(carList.isPresent()) {
-			return carList.get();
+		try {
+			if(carList.isPresent()) {
+				return carList.get();
+			}
+			else {
+				throw new ResourceNotFoundException("Record not found with ID :" + id);
+			}
 		}
-		else {
-			throw new ResourceNotFoundException("Record not found with ID :" + id);
+		catch(Exception e) {
+			throw new ModelNotFoundException("Couldnt find the appointment by id"+ id );
 		}
 	}
 
 	@Override
 	public List<Car> getAllCars() {
-		return (List<Car>) ICarRepository.findAll();
+		try {
+			return (List<Car>) ICarRepository.findAll();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			throw new ModelEmptyListException("error retriving appointments...please try again" );
+		}
 	}
 
 
 
 	@Override
 	public List<Car> getCarsByModel(String model) {
-		// TODO Auto-generated method stub
-		return (List<Car>) ICarRepository.findByModel(model);
+		try {
+			return (List<Car>) ICarRepository.findByModel(model);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			throw new ModelEmptyListException("error retriving appointments...please try again" );
+		}
 	}
 
 	@Override
 	public List<Car> getCarsByBrand(String brand) {
-		// TODO Auto-generated method stub
-		return (List<Car>) ICarRepository.findByBrand(brand);
+		try {
+			return (List<Car>) ICarRepository.findByBrand(brand);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			throw new ModelEmptyListException("error retriving appointments...please try again" );
+		}
 	}
 
 //	@Override
